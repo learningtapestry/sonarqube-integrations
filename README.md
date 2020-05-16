67bede753d835b981747777e912b6013535e62d5
Alexander Kuznetsov <paranoic.san@gmail.com>
2020-05-16 08:56:19 +0200

    Add ENV to customize the ruby scanner run
    Introduce new ENV variables:
    
    - `SONAR_RUBOCOP_REPORT` - contains file name with rubocop report.
    - `SONAR_TEST_COMMAND` - contains a command to be executed to start coverage file generation.
    
    Also, doesn't force execute rubocop or test coverage commands if there are report files for them. That can be if such analyses have been run before.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sonarqube integrations
-Convenience scripts to integrate SonarQube scanners. 
+Convenience scripts to integrate SonarQube scanners.
 They're meant to be used primarily in CI environments.
 
 ## JavasScript / TypeScript
@@ -12,6 +12,13 @@ wget -qO- https://raw.githubusercontent.com/learningtapestry/sonarqube-integrati
 ```
 
 ## Ruby
+
+ENV variables to customize setup:
+
+`SONAR_RUBOCOP_REPORT` contains name for rubocop report file to be catched up sonar scanner. Default is `rubocop-report.json`. If report file doesn't exist rubocop scanner is executed to generate the report file.
+
+If coverage file `coverage/.resultset.json` doesn't exist test command inside `SONAR_TEST_COMMAND` is executed. Default command is `bundle exec rake test || bundle exec rspec`.  Usually a command runs tests which generates a coverage.
+
 ```bash
 curl -o- https://raw.githubusercontent.com/learningtapestry/sonarqube-integrations/master/scanner-ruby.sh | bash
 ```
--- scanner-ruby.sh
+++ scanner-ruby.sh
@@ -11,13 +11,18 @@ export PATH=$PATH:/tmp/sonar-scanner-4.2.0.1873-linux/bin
 
 # Configure Learning Tapestry SonarQube server
 sed -i 's|#sonar.host.url=http://localhost:9000|sonar.host.url=https://sonarqube.learningtapestry.com|g' \
-  /tmp/sonar-scanner-4.2.0.1873-linux/conf/sonar-scanner.properties
+ /tmp/sonar-scanner-4.2.0.1873-linux/conf/sonar-scanner.properties
 
-# Generate Rubocop report
-bundle exec rubocop --format=json --out=rubocop-report.json || true
+# Generate Rubocop report if not exists
+report_file=${SONAR_RUBOCOP_REPORT:-'rubocop-report.json'}
+if [ ! -e "$report_file" ]; then
+  bundle exec rubocop --format=json --out="$report_file" || true
+fi
 
-# Generate test coverage report
-bundle exec rake test || bundle exec rspec
+# Generate test coverage report if none has been generated
+if [ ! -e 'coverage/.resultset.json' ]; then
+  eval "${SONAR_TEST_COMMAND:-'bundle exec rake test || bundle exec rspec'}"
+fi
 
 # Run scanner
 cd -P . || true # Needed because CodeShip stores the code using a symbolic link that gives problems to the scanner
